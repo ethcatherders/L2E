d5aabaf0903ebe1ef9ede54f4c633e48911c1d05
George <gbh218@gmail.com>
2022-04-11 13:45:58 -0400

    Add upload form for creating new courses

--- quizmaster/pages/courses/create.js
+++ quizmaster/pages/courses/create.js
@@ -18,7 +18,9 @@ import {
   AlertDescription,
   Box,
   Center,
-  Spinner
+  Spinner,
+  Input,
+  Heading
 } from "@chakra-ui/react";
 
 /*
@@ -26,10 +28,21 @@ import {
 */
 
 export default function CreateCourse() {
+  async function submitNewCourseFromFile(e) {
+    e.preventDefault();
+    // call a POST fetch request to API for parsing and appending to database
+  }
+
   return (
     <Layout>
       <Container paddingTop={5} paddingBottom={5}>
-        <h1>New Courses</h1>
+        <Heading mb={5}>Create a New Course</Heading>
+        <form method="POST" onSubmit={submitNewCourseFromFile}>
+          <FormControl>
+            <FormLabel>Upload a file ending in either .xls, .xlsx, .json or .csv</FormLabel>
+            <Input type='file' name="upload" accept=".xls,.xlsx,.json,.csv" />
+          </FormControl>
+        </form>
       </Container>
     </Layout>
   )
