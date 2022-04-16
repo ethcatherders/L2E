142135eeba32783f78b7df9680df21a6eff15fc4
George <gbh218@gmail.com>
2022-04-16 15:48:04 -0400

    Add upload form and parsing of .xlsx files for new courses

--- quizmaster/pages/courses/create.js
+++ quizmaster/pages/courses/create.js
@@ -1,36 +1,77 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useMoralis } from "react-moralis";
-import { useRouter } from 'next/router';
 import Layout from "../../components/Layout";
 import { 
   Container, 
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  RadioGroup,
-  Radio,
-  VStack,
   Button,
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-  Box,
-  Center,
-  Spinner,
+  Text,
   Input,
   Heading
 } from "@chakra-ui/react";
+import { read } from 'xlsx';
 
 /*
   Create a new course using a form.
 */
 
 export default function CreateCourse() {
+  const [error, setError] = useState(false);
+  const { Moralis } = useMoralis();
+
   async function submitNewCourseFromFile(e) {
     e.preventDefault();
     // call a POST fetch request to API for parsing and appending to database
+    try {
+      const fileList = document.getElementById('file').files;
+      console.log(fileList[0].name);
+      const fileReader = new FileReader();
+      fileReader.readAsBinaryString(fileList[0]);
+      fileReader.onload = async (e) => {
+        try {
+          const workbook = read(e.target.result, {type: 'binary'});
+          const sheetNames = workbook.SheetNames;
+          const courses = [];
+          for(let i=0; sheetNames.length > i; i++) {
+            const sheet = workbook.Sheets[sheetNames[i]];
+            const title = sheet.B1.v;
+            const videoUrl = sheet.B2.v;
+    
+            let active = true;
+            const quiz = [];
+            for(let x=5; active; x++) {
+              if (!sheet[`A${x}`]) break
+              const id = x - 4;
+              const question = sheet[`A${x}`].v;
+              const options = [sheet[`B${x}`].v, sheet[`C${x}`].v, sheet[`D${x}`].v, sheet[`E${x}`].v];
+              const answer = sheet[`F${x}`].v;
+              const quizItem = { id, question, options, answer };
+              quiz.push(quizItem);
+            }
+            courses.push({ title, videoUrl, quiz, responses: [] });
+          }
+    
+          console.log("Courses: ", courses);
+          for(let y=0; courses.length > y; y++) {
+            await uploadCourse(courses[y]);
+          }
+          console.log("Success!");
+        } catch (error) {
+          console.error(error);
+          setError(true);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    }
+  }
+
+  async function uploadCourse(courseData) {
+    const Course = Moralis.Object.extend("Course");
+    const course = new Course();
+    await course.save(courseData);
   }
 
   return (
@@ -39,9 +80,11 @@ export default function CreateCourse() {
         <Heading mb={5}>Create a New Course</Heading>
         <form method="POST" onSubmit={submitNewCourseFromFile}>
           <FormControl>
-            <FormLabel>Upload a file ending in either .xls, .xlsx, .json or .csv</FormLabel>
-            <Input type='file' name="upload" accept=".xls,.xlsx,.json,.csv" />
+            <FormLabel>Upload a file ending in .xlsx</FormLabel>
+            <Input type='file' id="file" name="upload" accept=".xlsx" />
           </FormControl>
+          {error ? <Text color="red">Something went wrong.</Text> : ''}
+          <Button type="submit" marginTop={5}>Submit</Button>
         </form>
       </Container>
     </Layout>
