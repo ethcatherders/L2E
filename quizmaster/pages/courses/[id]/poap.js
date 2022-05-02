a878ae00df10ed98051420987f2c81f5254d106d
George <gbh218@gmail.com>
2022-05-02 12:39:45 -0400

    Add form to attach POAP to course

--- quizmaster/pages/courses/[id]/poap.js
+++ quizmaster/pages/courses/[id]/poap.js
@@ -1,19 +1,104 @@
 import { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
-import { Container, AspectRatio, Box, Text, Button, Center, Heading, HStack } from '@chakra-ui/react';
+import { Container, Text, Button, Center, Heading, HStack, Select } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import Link from "next/link";
 
 import Layout from "../../../components/Layout";
 
 export default function Poap() {
+  const [course, setCourse] = useState({});
+  const [poaps, setPoaps] = useState([]);
+  const [assignedPoapId, setAssignedPoapId] = useState(null);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { isInitialized, Moralis } = useMoralis();
+  const router = useRouter();
 
   useEffect(async () => {
+    if (isInitialized) {
+      await getCourse();
+      await getPoaps();
+    }
+  }, [isInitialized, !!router.query.id]);
 
-  }, [isInitialized]);
+  async function courseFromMoralis() {
+    const Course = Moralis.Object.extend("Course");
+    const query = new Moralis.Query(Course);
+    const { id } = router.query;
+    return await query.get(id);
+  }
+
+  async function getCourse() {
+    try {
+      const result = await courseFromMoralis();
+      console.log(result)
+      return setCourse(result.attributes);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getPoaps() {
+    try {
+      const POAP = Moralis.Object.extend("POAP");
+      const query = new Moralis.Query(POAP);
+      const result = await query.find();
+      console.log(result);
+      setPoaps(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function submitAttachment(e) {
+    e.preventDefault();
+
+    setSuccess(false);
+    setError(false);
+    setLoading(true);
+
+    try {
+      if (!assignedPoapId) throw Error('No POAP was assigned');
+      const POAP = Moralis.Object.extend("POAP");
+      const query = new Moralis.Query(POAP);
+      const poap = await query.get(assignedPoapId);
+      const currentCourse = await courseFromMoralis();
+      poap.set("course", currentCourse);
+      await poap.save();
+      setSuccess(true)
+    } catch (error) {
+      console.error(error);
+      setError(true)
+    }
+    setLoading(false)
+  }
 
   return (
-    <Layout></Layout>
+    <Layout>
+      <Container>
+        <HStack mt={5} mb={10} justifyContent="space-between">
+          <Heading>Attach POAP</Heading>
+          <Link href={`/courses/${router.query.id}`} passHref>
+            <Button>Go Back</Button>
+          </Link>
+        </HStack>
+        <form onSubmit={submitAttachment}>
+          <Text textAlign="center">Attach a POAP to the following course:</Text>
+          <Heading size="lg" textAlign="center">{course.title}</Heading>
+          <Select mt={5} placeholder="Select one" onChange={(e) => setAssignedPoapId(e.currentTarget.value)}>
+            {poaps.map((poap, index) => 
+              <option value={poap.id} key={index}>{poap.attributes.name}</option>
+            )}
+          </Select>
+          {error ? <Text color="red">Something went wrong.</Text> : ''}
+          {success ? <Text color="green">The POAP has been assigned!</Text> : ''}
+          <Center mt={5}>
+            <Button type="submit" isLoading={loading}>Attach</Button>
+          </Center>
+        </form>
+      </Container>
+    </Layout>
   )
 }
