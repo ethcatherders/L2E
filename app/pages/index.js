8f2b6f88ebc3243301a002beae54b1dacaa04e4d
George <gbh218@gmail.com>
2022-08-07 21:02:51 -0400

    Add speaker name, avatar and video duration UI elements

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Container, Box, Text, Center, Heading, Image, AspectRatio, Flex, Skeleton, useColorMode } from '@chakra-ui/react';
+import { Grid, GridItem, Container, Box, Text, Center, Heading, Image, AspectRatio, Flex, Skeleton, useColorMode, Circle, Avatar } from '@chakra-ui/react';
 import NextImage from 'next/image';
 import NextLink from 'next/link';
 import styles from '../styles/Home.module.css';
@@ -45,12 +45,12 @@ export default function Home() {
       <Container maxW='container.xl' paddingTop={5} paddingBottom={5}>
         {courses.length ?
           <NextLink href={`/courses/${courses[0].id}`}>
-            <Flex justify='center' align='center' id='showcase' mb={10} borderRadius="2xl" width="100%" height={250} border="1px solid grey" cursor="pointer" overflow='hidden'>
+            <Flex direction='column' justify='center' align='center' id='showcase' mb={10} borderRadius="2xl" width="100%" height={250} border="1px solid grey" cursor="pointer" overflow='hidden'>
               <Center
                 zIndex={1}
                 position='absolute'
               >
-                <Heading size='xl' noOfLines={1}>{courses[0].title}</Heading>
+                <Heading size='xl' noOfLines={1} color='white'>{courses[0].title}</Heading>
               </Center>
               <Image
                 src={courses[0].thumbnail}
@@ -64,6 +64,43 @@ export default function Home() {
                 borderRadius="2xl"
                 _hover={{ transform: 'scale(1.2)' }}
               />
+              <Flex width='100%' justify='start' align='center'>
+                <Flex position='absolute' gap={4} justify='center' align='center' mt={-76} ml={10}>
+                  <Circle
+                    border='1px solid grey'
+                    padding={1}
+                  >
+                    <Avatar
+                      src={courses[0].speakerImg}
+                      height={50}
+                      width={50}
+                    />
+                  </Circle>
+                  <Heading
+                    size='xs'
+                    noOfLines={1}
+                    color='white'
+                  >
+                    {courses[0].speaker ?? 'Unknown'}
+                  </Heading>
+                </Flex>
+              </Flex>
+              <Flex width='100%' justify='end'>
+                <Text
+                  position='absolute'
+                  textAlign='right'
+                  paddingX={2}
+                  paddingY={0.25}
+                  borderRadius={10}
+                  background={colorMode === 'dark' ? 'rgba(0,0,0,0.8)' : 'rgba(254, 194, 113, 0.8)'}
+                  color={colorMode === 'dark' ? 'white' : 'rgba(70, 69, 67, 1)'}
+                  mr={4}
+                  mt={-10}
+                  zIndex={1}
+                >
+                  {courses[0].duration ?? '-- min'}
+                </Text>
+              </Flex>
             </Flex>
           </NextLink>
           :
@@ -72,7 +109,33 @@ export default function Home() {
         <Grid templateColumns={'repeat(auto-fit, minmax(200px, 1fr))'} gap={5}>
           {courses.slice(1).map((course, index) =>
             <NextLink href={`/courses/${course.id}`} key={index}>
-              <GridItem bg="rgba(36, 39, 48, 1)" borderRadius='2xl' maxWidth='350px' minHeight='fit-content' border='1px solid grey' cursor='pointer' overflow='hidden'>
+              <GridItem
+                // display='flex'
+                // flexDir='column'
+                bg="rgba(36, 39, 48, 1)"
+                borderRadius='2xl'
+                maxWidth='350px'
+                minHeight='fit-content'
+                border='1px solid grey'
+                cursor='pointer'
+                overflow='hidden'
+              >
+                <Flex width='100%' justify='end'>
+                  <Text
+                    position='fixed'
+                    textAlign='right'
+                    paddingX={2}
+                    paddingY={0.25}
+                    borderRadius={10}
+                    background={colorMode === 'dark' ? 'rgba(0,0,0,0.8)' : 'rgba(254, 194, 113, 0.8)'}
+                    color={colorMode === 'dark' ? 'white' : 'rgba(70, 69, 67, 1)'}
+                    mr={2}
+                    mt={2}
+                    zIndex={1}
+                  >
+                    {course.duration ?? '-- min'}
+                  </Text>
+                </Flex>
                 <AspectRatio overflow='hidden' borderTopRadius='2xl' ratio={1.75}>
                   <Image
                     src={course.thumbnail}
@@ -85,14 +148,42 @@ export default function Home() {
                 </AspectRatio>
                 <Flex
                   direction='column'
-                  justify='center'
+                  // justify='center'
+                  align='end'
                   padding={4}
                   borderBottomRadius='2xl'
                   height={100}
                   zIndex={1}
                   bg={colorMode === 'dark' ? "rgba(36, 39, 48, 1)" : "rgba(196, 196, 196, 1)"}
                 >
-                  <Heading size='sm' noOfLines={3}>{course.title}</Heading>
+                  <Circle
+                    position='absolute'
+                    mt={-10}
+                    border='1px solid grey'
+                    padding={1}
+                  >
+                    <Avatar
+                      src={course.speakerImg}
+                      height={50}
+                      width={50}
+                    />
+                  </Circle>
+                  <Heading
+                    size='xs'
+                    noOfLines={1}
+                    alignSelf='start'
+                    color={colorMode === 'dark' ? 'rgba(183, 185, 210, 1)' : 'rgba(70, 69, 67, 1)'}
+                  >
+                    {course.speaker ?? 'Unknown'}
+                  </Heading>
+                  <Heading
+                    size='sm'
+                    noOfLines={3}
+                    alignSelf='start'
+                    mt={2}
+                  >
+                    {course.title}
+                  </Heading>
                 </Flex>
               </GridItem>
             </NextLink>
