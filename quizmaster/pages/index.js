95f4e2867754fb0ccf095d62e130ab0e8aca9ab2
George <gbh218@gmail.com>
2022-08-26 18:06:51 -0400

    Add dates and editors info

--- quizmaster/pages/index.js
+++ quizmaster/pages/index.js
@@ -1,4 +1,4 @@
-import { Grid, HStack, GridItem, Container, Box, Text, Center, Heading, Image, AspectRatio, Flex, Skeleton, Circle, Avatar, Button } from '@chakra-ui/react';
+import { Grid, HStack, GridItem, Container, Box, Text, Center, Heading, Image, AspectRatio, Flex, Skeleton, Circle, Avatar, Button, VStack } from '@chakra-ui/react';
 import NextImage from 'next/image';
 import NextLink from 'next/link';
 import styles from '../styles/Home.module.css';
@@ -34,9 +34,14 @@ export default function Home() {
 
       return {
         id: course.id,
-        title: course.attributes.title,
+        speaker: course.attributes.speaker,
+        speakerImg: course.attributes.speakerImg,
+        duration: course.attributes.duration,
         thumbnail: thumbnailUrl ? thumbnailUrl : pic,
-        completed: false
+        createdAt: course.createdAt,
+        createdBy: course.attributes.createdBy,
+        updatedAt: course.updatedAt,
+        updatedBy: course.attributes.updatedBy
       }
     });
     setCourses(results);
@@ -53,92 +58,112 @@ export default function Home() {
         </HStack>
         <Grid templateColumns={'repeat(auto-fit, minmax(200px, 1fr))'} gap={5}>
           {courses.map((course, index) =>
-            <NextLink href={`/courses/${course.id}`} key={index}>
-              <GridItem
-                // display='flex'
-                // flexDir='column'
-                bg="rgba(36, 39, 48, 1)"
-                borderRadius='2xl'
-                maxWidth='350px'
-                minHeight='fit-content'
-                border='1px solid grey'
-                cursor='pointer'
-                overflow='hidden'
-              >
-                <Flex width='100%' justify='end'>
-                  <Text
-                    position='fixed'
-                    textAlign='right'
-                    paddingX={2}
-                    paddingY={0.25}
-                    borderRadius={10}
-                    background={'rgba(0,0,0,0.8)'}
-                    color={'white'}
-                    mr={2}
-                    mt={2}
-                    zIndex={1}
-                  >
-                    {course.duration ?? '-- min'}
-                  </Text>
-                </Flex>
-                {course.thumbnail === pic ? (
-                  <Image
-                    src={course.thumbnail}
-                    objectFit='cover'
-                  />
-                ) : (
-                  <AspectRatio overflow='hidden' borderTopRadius='2xl' ratio={1.75}>
+            <GridItem>
+              <NextLink href={`/courses/${course.id}`} key={index}>
+                <GridItem
+                  // display='flex'
+                  // flexDir='column'
+                  bg="rgba(36, 39, 48, 1)"
+                  borderRadius='2xl'
+                  maxWidth='350px'
+                  minHeight='fit-content'
+                  border='1px solid grey'
+                  cursor='pointer'
+                  overflow='hidden'
+                >
+                  <Flex width='100%' justify='end'>
+                    <Text
+                      position='fixed'
+                      textAlign='right'
+                      paddingX={2}
+                      paddingY={0.25}
+                      borderRadius={10}
+                      background={'rgba(0,0,0,0.8)'}
+                      color={'white'}
+                      mr={2}
+                      mt={2}
+                      zIndex={1}
+                    >
+                      {course.duration ?? '-- min'}
+                    </Text>
+                  </Flex>
+                  {course.thumbnail === pic ? (
                     <Image
                       src={course.thumbnail}
-                      fallback={<Skeleton width='100%' height='100%' />}
                       objectFit='cover'
-                      transition='all .5s ease'
-                      _hover={{ transform: 'scale(1.2)' }}
-                      zIndex={0}
                     />
-                  </AspectRatio>
-                )}
-                <Flex
-                  direction='column'
-                  // justify='center'
-                  align='end'
-                  padding={4}
-                  borderBottomRadius='2xl'
-                  minHeight={100}
-                  zIndex={1}
-                  bg={"rgba(36, 39, 48, 1)"}
-                >
-                  <Circle
-                    position='absolute'
-                    mt={-10}
-                    border='1px solid grey'
-                    padding={1}
+                  ) : (
+                    <AspectRatio overflow='hidden' borderTopRadius='2xl' ratio={1.75}>
+                      <Image
+                        src={course.thumbnail}
+                        fallback={<Skeleton width='100%' height='100%' />}
+                        objectFit='cover'
+                        transition='all .5s ease'
+                        _hover={{ transform: 'scale(1.2)' }}
+                        zIndex={0}
+                      />
+                    </AspectRatio>
+                  )}
+                  <Flex
+                    direction='column'
+                    // justify='center'
+                    align='end'
+                    padding={4}
+                    borderBottomRadius='2xl'
+                    minHeight={100}
+                    zIndex={1}
+                    bg={"rgba(36, 39, 48, 1)"}
                   >
-                    <Avatar
-                      src={course.speakerImg}
-                      height={50}
-                      width={50}
-                    />
-                  </Circle>
-                  <Heading
-                    size='xs'
-                    noOfLines={1}
-                    alignSelf='start'
-                    color={'rgba(183, 185, 210, 1)'}
-                  >
-                    {course.speaker ?? 'Unknown'}
-                  </Heading>
-                  <Heading
-                    size='sm'
-                    noOfLines={3}
-                    alignSelf='start'
-                    mt={2}
-                  >
-                    {course.title}
-                  </Heading>
-                </Flex>
-              </GridItem>
-            </NextLink>
+                    <Circle
+                      position='absolute'
+                      mt={-10}
+                      border='1px solid grey'
+                      padding={1}
+                    >
+                      <Avatar
+                        src={course.speakerImg}
+                        height={50}
+                        width={50}
+                      />
+                    </Circle>
+                    <Heading
+                      size='xs'
+                      noOfLines={1}
+                      alignSelf='start'
+                      color={'rgba(183, 185, 210, 1)'}
+                    >
+                      {course.speaker ?? 'Unknown'}
+                    </Heading>
+                    <Heading
+                      size='sm'
+                      noOfLines={3}
+                      alignSelf='start'
+                      mt={2}
+                    >
+                      {course.title}
+                    </Heading>
+                  </Flex>
+                </GridItem>
+              </NextLink>
+              <HStack fontSize={10} width='100%' mt={2}>
+                <VStack align='left'>
+                  <Text padding={1} borderRadius={5} background='#242730' textColor='white'>
+                    Created at: {course.createdAt.toLocaleDateString()}
+                  </Text>
+                  <Text padding={1} borderRadius={5} background='#242730' textColor='white'>
+                    Updated at: {course.updatedAt.toLocaleDateString()}
+                  </Text>
+                </VStack>
+                <VStack align='left'>
+                  <Text padding={1} borderRadius={5} background='#242730' textColor='white'>
+                    Created by: {course.createdBy ? course.createdBy.getUsername() : '--'}
+                  </Text>
+                  <Text padding={1} borderRadius={5} background='#242730' textColor='white'>
+                    Updated by: {course.updatedBy ? course.updatedBy.getUsername() : '--'}
+                  </Text>
+                </VStack>
+              </HStack>
+            </GridItem>
           )}
         </Grid>
       </Container>
