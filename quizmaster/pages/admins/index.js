658195c37881bc602bc2d989d0847bfe7c54f579
George <gbh218@gmail.com>
2022-08-28 23:39:28 -0400

    Add cloud functions and admin page functions

--- quizmaster/pages/admins/index.js
+++ quizmaster/pages/admins/index.js
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, Heading, Input, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, FormControl, Heading, Input, Text, VStack, HStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
 import Layout from "../../components/Layout";
@@ -24,9 +24,8 @@ export default function Admins() {
 
   async function getAdmins() {
     try {
-      const Admin = Moralis.Object.extend("Admin")
-      const query = new Moralis.Query(Admin)
-      const results = await query.find()
+      const results = await Moralis.Cloud.run("getAdmins")
+      if (!results) return
       setAdmins(results)
     } catch (error) {
       console.error(error)
@@ -35,20 +34,23 @@ export default function Admins() {
 
   async function search() {
     try {
-      const query = new Moralis.Query(Moralis.User)
-      if (searchInput.trim().startsWith('0x')) {
-        query.equalTo("ethAddress", searchInput)
-      } else {
-        query.equalTo("username", searchInput)
+      const params = {username: searchInput.trim(), ethAddress: searchInput.trim()}
+
+      const results = await Moralis.Cloud.run("searchForUser", params)
+      if (results && results.length) {
+        setSearchResults([...results])
       }
-      const results = await query.map(result => {
-        return {
-          id: result.id,
-          username: result.getUsername(),
-          ethAddress: result.attributes.ethAddress
-        }
-      })
-      setSearchResults([...results])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function addAdmin(id) {
+    try {
+      const params = { id }
+      const response = await Moralis.Cloud.run("addAdmin", params)
+      console.log("Added admin:", response.success)
+      await getAdmins()
     } catch (error) {
       console.error(error)
     }
@@ -69,18 +71,30 @@ export default function Admins() {
         {(searchResults.length > 0 && searchInput) && (
           <Box position='absolute' bg='rgba(36, 39, 48, 1)' borderRadius={10}>
             {searchResults.map((result, index) => (
-              <Box key={index} padding={4} borderRadius={10}>
-                <Text>{result.username}</Text>
-                <Text>{result.ethAddress}</Text>
-              </Box>
+              <HStack
+                key={index}
+                padding={4}
+                borderRadius={10}
+                justify='space-between'
+                align='center'
+                gap={10}
+              >  
+                <Box>
+                  <Text>{result.username}</Text>
+                  <Text>{result.ethAddress}</Text>
+                </Box>
+                <Button onClick={() => addAdmin(result.id)}>
+                  Add Admin
+                </Button>
+              </HStack>
             ))}
           </Box>
         )}
         <VStack align='left' width='100%' mt={5}>
           {admins.map(admin => (
-            <Box padding={2} border='1px solid grey' borderRadius={5}>
-              <Text fontWeight='bold'>{admin.attributes.user.getUsername()}</Text>
-              <Text>{admin.attributes.user.attributes.ethAddress}</Text>
+            <Box padding={2} border='1px solid grey' borderRadius={5} key={admin.id}>
+              <Text fontWeight='bold'>{admin.username}</Text>
+              <Text>{admin.ethAddress}</Text>
             </Box>
           ))}
         </VStack>
