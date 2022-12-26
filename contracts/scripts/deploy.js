277457420d680f36c4c74b668f46ad9f7b772542
George <gbh218@gmail.com>
2022-12-26 16:21:11 -0500

    Add implementation and logs for deployment

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -5,16 +5,36 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+require("dotenv").config();
 
 async function main() {
+  const [signer] = await hre.ethers.getSigners()
+
+  console.log("Deploying Reward...")
+  const Reward = await hre.ethers.getContractFactory("Reward");
+  const reward = await Reward.deploy(signer.address);
+  await reward.deployed()
+
+  console.log(
+    `Reward deployed to ${reward.address}`,
+    `\nOwnership of Reward assigned to ${await reward.owner()}`
+  );
+
+  const initLockTx = await reward.initLock();
+  await initLockTx.wait();
+
+  console.log(`\nReward contract has been initlocked.\n`)
+  console.log("Deploying RewardFactory...")
+
   const RewardFactory = await hre.ethers.getContractFactory("RewardFactory");
-  const factory = await RewardFactory.deploy();
+  const factory = await RewardFactory.deploy(reward.address);
 
   await factory.deployed();
 
   console.log(
     `RewardFactory deployed to ${factory.address}`,
-    `Ownership assigned to ${await factory.owner()}`
+    `\nImplementation set as Reward address: ${await factory.implementation()}`,
+    `\nOwnership of RewardFactory assigned to ${await factory.owner()}`
   );
 }
 
