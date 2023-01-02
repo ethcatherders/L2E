ec2b04a9f22874c24ccaddaba44005fc7c9d678e
George <gbh218@gmail.com>
2023-01-01 22:43:53 -0500

    Add icons and detect + switch networks

--- app/components/NavBar/index.js
+++ app/components/NavBar/index.js
@@ -22,27 +22,102 @@ import {
   MenuList,
   MenuItem,
   Circle,
-  IconButton
+  IconButton,
+  Tag,
+  TagLabel,
+  Tooltip
 } from '@chakra-ui/react';
-import { useContext, useRef } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { useMoralis } from 'react-moralis';
-import { EvmChain } from '@moralisweb3/common-evm-utils';
 import { Web3Context } from '../../context/Web3Context';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { MoonIcon, SunIcon } from '../Icons';
 
 
 export default function NavBar(props) {
+  const [wrongNetworkMsg, setWrongNetworkMsg] = useState()
   const { devMode, setDevMode } = useContext(Web3Context)
-  const { authenticate, isAuthenticating, logout, isLoggingOut, isAuthenticated, user } = useMoralis();
+  const { authenticate, isAuthenticating, logout, Moralis, isAuthenticated, user, isInitialized, chainId } = useMoralis();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = useRef();
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (user && isAuthenticated) {
+      getNetwork()
+    }
+  }, [isInitialized, user, isAuthenticated, chainId, devMode])
+
+  async function getNetwork() {
+    const connectorType = Moralis.connectorType;
+    if (connectorType === "injected") {
+      if ((!devMode && chainId !== "0x89") || (devMode && chainId !== "0x13881")) {
+        setWrongNetworkMsg(true)
+      } else {
+        setWrongNetworkMsg(false)
+      }
+    }
+  }
+
+  async function switchNetwork() {
+    if (devMode) {
+      await Moralis.switchNetwork("0x13881")
+      if (chainId === "0x13881") {
+        const chainId = 80001;
+        const chainName = "Mumbai Testnet";
+        const currencyName = "MATIC";
+        const currencySymbol = "MATIC";
+        const rpcUrl = "https://rpc-mumbai.maticvigil.com/";
+        const blockExplorerUrl = "https://mumbai.polygonscan.com/";
+  
+        await Moralis.addNetwork(
+          chainId,
+          chainName,
+          currencyName,
+          currencySymbol,
+          rpcUrl,
+          blockExplorerUrl
+        );
+      }
+    } else {
+      await Moralis.switchNetwork("0x89")
+      if (chainId === "0x89") {
+        const chainId = 137;
+        const chainName = "Polygon Mainnet";
+        const currencyName = "MATIC";
+        const currencySymbol = "MATIC";
+        const rpcUrl = "https://polygon-rpc.com";
+        const blockExplorerUrl = "https://polygonscan.com/";
+  
+        await Moralis.addNetwork(
+          chainId,
+          chainName,
+          currencyName,
+          currencySymbol,
+          rpcUrl,
+          blockExplorerUrl
+        );
+      }
+    }
+  }
 
   return (
     <Box>
       <HStack justifyContent="flex-end" alignItems='center' padding={5}>
+        {wrongNetworkMsg && (
+          <Tooltip label={`Switch to ${devMode ? 'Mumbai Testnet' : 'Polygon Mainnet'}`} aria-label='A tooltip'>
+            <Tag
+              colorScheme='red'
+              borderRadius='full'
+              padding={2}
+              cursor='pointer'
+              onClick={switchNetwork}
+            >
+              <TagLabel>Wrong Network</TagLabel>
+            </Tag>
+          </Tooltip>
+        )}
         <IconButton
-          icon={colorMode === 'dark' ? <MoonIcon/> : <SunIcon/>}
+          icon={colorMode === 'dark' ? <MoonIcon /> : <SunIcon/>}
           color='white'
           bg='rgba(35, 35, 35, 1)'
           onClick={toggleColorMode}
